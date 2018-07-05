d5b065af99d3845790635fe73dbb7e4ecfe39964
0xD34DC0DE <emerickpoulin31415@gmail.com>
2018-07-05 12:26:49 -0400

    Improved visualisation

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     filename = 'data/puzzle.txt'
 
     file1 = load_puzzle(filename)
-    data = decode_file(file1)
-    show_puzzle(data)
-    print(data)
+    puzzle = decode_file(file1)
+    show_puzzle(puzzle)
+    print(puzzle)
--- src/utils/visual.py
+++ src/utils/visual.py
@@ -1,13 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 
 def show_puzzle(puzzle: np.array):
     fig, ax = plt.subplots()
-    for i in range(10):
-        for j in range(10):
-            c = puzzle[j, i]
-            if c != 0:
-                ax.text(i, j, str(c - 1), va='center', ha='center')
-    ax.matshow(puzzle, cmap='Blues')
+
+    for (i, j), z in np.ndenumerate(puzzle):
+        if z != 0:
+            ax.text(j, i, '{:0.0f}'.format(z - 1), ha='center', va='center')
+
+    ax.matshow(puzzle, cmap='Paired')
+    minor_ticks = [.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5]
+    ax.set_xticks(minor_ticks, minor=True)
+    ax.set_yticks(minor_ticks, minor=True)
+    ax.grid(which='minor', linewidth=1.1, color='k')
+
+    # Set major ticks locations
+    major_ticks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ax.xaxis.set_major_locator(ticker.FixedLocator(major_ticks))
+    ax.yaxis.set_major_locator(ticker.FixedLocator(major_ticks))
+
+    # Customize minor tick labels
+    ax.xaxis.set_minor_locator(ticker.FixedLocator(minor_ticks))
+    ax.yaxis.set_minor_locator(ticker.FixedLocator(minor_ticks))
+
+    char_array = np.arange(10)
+
+    # Strip the '.0' and convert to string representation of number
+    char_array = [str(x) for x in char_array]
+
+    # Set major axis labels
+    ax.xaxis.set_major_formatter(ticker.FixedFormatter(char_array))
+    ax.yaxis.set_major_formatter(ticker.FixedFormatter(char_array))
+
     plt.show()
